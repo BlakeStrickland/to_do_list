4e2f6213203dfac3decb406b884e6ee97ac86222
Anthony Ellis <ajellis1989@gmail.com>
2015-10-16 11:52:25 -0400

    Adds 7 tasks to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Task.create!(description: "Take Out The Trash", due_on: Date.today)
+Task.create!(description: "Wash the dishes", due_on: Date.today)
+Task.create!(description: "Cook dinner for the kids", due_on: Date.today)
+Task.create!(description: "Finish battleship (-___-)", due_on: Date.today)
+Task.create!(description: "Eat ice cream", due_on: Date.today)
+Task.create!(description: "Practice music", due_on: Date.today)
+Task.create!(description: "make cookies for Rails classmates", due_on: Date.today + 2.days)
