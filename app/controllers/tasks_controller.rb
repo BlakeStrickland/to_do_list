117225fd1fb1cf8dba2480edf94af778a74899b4
Blake Strickland <Stricklandb02@gmail.com>
2015-10-16 12:26:44 -0400

    Update seeds and strong params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,6 +53,6 @@ class TasksController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def task_params
-      params.require(:task).permit(:description, :due_on)
+      params.require(:task).permit(:description, :due_on, :priority)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Task.create!(description: "Take Out The Trash", due_on: Date.today)
-Task.create!(description: "Wash the dishes", due_on: Date.today)
-Task.create!(description: "Cook dinner for the kids", due_on: Date.today)
-Task.create!(description: "Finish battleship (-___-)", due_on: Date.today)
-Task.create!(description: "Eat ice cream", due_on: Date.today)
-Task.create!(description: "Practice music", due_on: Date.today)
-Task.create!(description: "make cookies for Rails classmates", due_on: Date.today + 2.days)
+Task.create!(description: "Take Out The Trash", due_on: Date.today, priority: "4")
+Task.create!(description: "Wash the dishes", due_on: Date.today, priority: "3")
+Task.create!(description: "Cook dinner for the kids", due_on: Date.today, priority: "2")
+Task.create!(description: "Finish battleship (-___-)", due_on: Date.today, priority: "5")
+Task.create!(description: "Eat ice cream", due_on: Date.today, priority: "6")
+Task.create!(description: "Practice music", due_on: Date.today, priority: "1")
+Task.create!(description: "make cookies for Rails classmates", due_on: Date.today + 2.days, priority: "7")
