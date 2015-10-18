cad244bd5fd117acad0edb363a8dde189257fc47
Blake Strickland <Stricklandb02@gmail.com>
2015-10-17 22:18:18 -0400

    Move from complete to incomplete on same page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016170021) do
+ActiveRecord::Schema.define(version: 20151017224430) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151016170021) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "priority"
+    t.boolean  "complete"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Task.create!(description: "Take Out The Trash", due_on: Date.today, priority: "4")
-Task.create!(description: "Wash the dishes", due_on: Date.today, priority: "3")
-Task.create!(description: "Cook dinner for the kids", due_on: Date.today, priority: "2")
-Task.create!(description: "Finish battleship (-___-)", due_on: Date.today, priority: "5")
-Task.create!(description: "Eat ice cream", due_on: Date.today, priority: "6")
-Task.create!(description: "Practice music", due_on: Date.today, priority: "1")
-Task.create!(description: "make cookies for Rails classmates", due_on: Date.today + 2.days, priority: "7")
+Task.create!(description: "Take Out The Trash", due_on: Date.today, priority: "4", complete: false)
+Task.create!(description: "Wash the dishes", due_on: Date.today, priority: "3", complete: false)
+Task.create!(description: "Cook dinner for the kids", due_on: Date.today, priority: "2", complete: false)
+Task.create!(description: "Finish battleship (-___-)", due_on: Date.today, priority: "5", complete: false)
+Task.create!(description: "Eat ice cream", due_on: Date.today, priority: "6", complete: false)
+Task.create!(description: "Practice music", due_on: Date.today, priority: "1", complete: false)
+Task.create!(description: "make cookies for Rails classmates", due_on: Date.today + 2.days, priority: "7", complete: false)
